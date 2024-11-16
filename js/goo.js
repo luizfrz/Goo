b99eacb58f45c0ccff6324cb314c60b9b8a7be0c
luiz frança <126346291+luizfrz@users.noreply.github.com>
2024-11-15 23:20:41 -0300

    try another script

--- js/goo.js
+++ js/goo.js
@@ -1,9 +1,1 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true
-    });
-});
+
